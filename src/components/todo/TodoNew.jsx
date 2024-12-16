2eb78d32ae584336388bb5aa5bbce8a3479b2eb6
TrienVo397 <s3907397@rmit.edu.vn>
2024-12-16 11:53:19 +0700

    #36 DOM events

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,15 +1,25 @@
 
 const TodoNew = (props) => {
-    console.log("Check props",props)
+    console.log("Check props", props)
 
-    const {addNewTodo} = props;
+    const { addNewTodo } = props;
     // addNewTodo("eric")
+    const handleClick = () => {
+        alert("You have clicked me")
+    }
 
-    
+    const handleOnChange = (name) => {
+        console.log("Value: ", name)
+    }
+
+
     return (
         <div className="todo-new">
-            <input type="text" placeholder="Add new task" />
-            <button>Add</button>
+            <input type="text" placeholder="Add new task"
+                onChange={(event) => handleOnChange(event.target.value)
+
+                } />
+            <button style={{ cursor: 'pointer' }} onClick={handleClick}>Add</button>
         </div>
     )
 }
